4f2635aaf8c7df0ba34c657d5f5b8fd55b14cccb
joon0708 <0708joon@gmail.com>
2025-09-01 10:07:02 +0900

    네임 스페이스 제거

--- turtlebot3_node/src/odometry.cpp
+++ turtlebot3_node/src/odometry.cpp
@@ -41,7 +41,7 @@ Odometry::Odometry(
 
   nh_->declare_parameter<std::string>("odometry.frame_id");
   nh_->declare_parameter<std::string>("odometry.child_frame_id");
-  nh_->get_parameter_or<std::string>("namespace", name_space_, std::string(""));
+  nh_->declare_parameter<std::string>("namespace");
 
   nh_->declare_parameter<bool>("odometry.use_imu");
   nh_->declare_parameter<bool>("odometry.publish_tf");
