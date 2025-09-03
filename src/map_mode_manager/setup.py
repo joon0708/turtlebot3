752635e894f7df77e929b7c6b7ec5dcf4deb9418
joon0708 <0708joon@gmail.com>
2025-09-03 17:11:03 +0900

    setup 수정

--- src/map_mode_manager/setup.py
+++ src/map_mode_manager/setup.py
@@ -14,6 +14,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
         (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
+        ('lib/' + package_name, []),  # libexec 디렉토리 생성
     ],
     install_requires=['setuptools'],
     zip_safe=True,
