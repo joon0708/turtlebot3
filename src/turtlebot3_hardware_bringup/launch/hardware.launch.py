d2d9f708144f14738565f6155b16dda5364a5aef
joon0708 <0708joon@gmail.com>
2025-09-03 14:20:38 +0900

    파라미터 경로 수정

--- src/turtlebot3_hardware_bringup/launch/hardware.launch.py
+++ src/turtlebot3_hardware_bringup/launch/hardware.launch.py
@@ -100,10 +100,8 @@ def generate_launch_description():
             executable='turtlebot3_ros',
             name='turtlebot3_hardware_node',  # 고유한 이름으로 변경
             output='screen',
-            parameters=[
-                '/root/turtlebot3/install/turtlebot3_bringup/share/turtlebot3_bringup/param/humble/waffle_pi_4wheel.yaml',
-                {'use_sim_time': use_sim_time}
-            ],
+            parameters=[{'use_sim_time': use_sim_time}],
+            params_file=['/root/turtlebot3/install/turtlebot3_bringup/share/turtlebot3_bringup/param/humble/waffle_pi_4wheel.yaml'],
             arguments=['-i', usb_port],
             env={
                 'TURTLEBOT3_MODEL': TURTLEBOT3_MODEL,
