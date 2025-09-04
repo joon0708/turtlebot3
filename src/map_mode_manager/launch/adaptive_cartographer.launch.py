e3997719dc21407b1073598a7bc080eb224bab01
joon0708 <0708joon@gmail.com>
2025-09-04 09:56:20 +0900

    환경변수 제거

--- src/map_mode_manager/launch/adaptive_cartographer.launch.py
+++ src/map_mode_manager/launch/adaptive_cartographer.launch.py
@@ -141,12 +141,7 @@ def generate_launch_description():
         arguments=[
             '-configuration_directory', cartographer_config_dir,
             '-configuration_basename', cartographer_config_basename,
-        ],
-        env={
-            'ROS_DOMAIN_ID': '10',
-            'ROS_VERSION': '2',
-            'ROS_DISTRO': 'humble'
-        }
+        ]
     )
     
     # Cartographer OccupancyGrid 노드
@@ -158,12 +153,7 @@ def generate_launch_description():
         parameters=[{
             'use_sim_time': use_sim_time,
             'publish_period_sec': 1.0,
-        }],
-        env={
-            'ROS_DOMAIN_ID': '10',
-            'ROS_VERSION': '2',
-            'ROS_DISTRO': 'humble'
-        }
+        }]
     )
     
     # RViz2 노드 (선택적)
