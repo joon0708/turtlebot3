aab6982dd8e3570516e55567ef9cc0b28558f516
joon0708 <0708joon@gmail.com>
2025-09-04 09:45:58 +0900

    방식 변경

--- src/map_mode_manager/setup.py
+++ src/map_mode_manager/setup.py
@@ -22,11 +22,4 @@ setup(
     description='Adaptive SLAM/Localization mode switching system for TurtleBot3',
     license='Apache License 2.0',
     tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            'map_comparator = map_mode_manager.map_comparator:main',
-            'map_similarity_checker = map_mode_manager.map_similarity_checker:main',
-            'mode_switcher = map_mode_manager.mode_switcher:main',
-        ],
-    },
 )
