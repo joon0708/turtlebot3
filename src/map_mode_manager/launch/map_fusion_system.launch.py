132d50f57931ee3e51e98598c8ec66e0d4a941fc
joon0708 <0708joon@gmail.com>
2025-09-05 09:00:18 +0900

    카토그래퍼 쪽 설정 변경

--- src/map_mode_manager/launch/map_fusion_system.launch.py
+++ src/map_mode_manager/launch/map_fusion_system.launch.py
@@ -92,6 +92,7 @@ def generate_launch_description():
             parameters=[{
                 'use_sim_time': use_sim_time,
             }],
+            arguments=['-resolution', '0.05', '-publish_period_sec', '1.0'],
             remappings=[
                 ('/map', '/cartographer_map')
             ]
